ac1c04c79bb7cf221d5f22b9ac731368b6da9235
Ramavtar-Nagar <121015446+Ramavtar-Nagar@users.noreply.github.com>
2024-06-22 17:48:43 +0530

    Update routing setup using createBrowserRouter and createRoutesFromElements for better route management

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About_Us/About.jsx'
@@ -30,6 +30,17 @@ import Contact from './components/Contact_Us/Contact.jsx'
 //     ]
 //   }
 // ])
+
+// Approach 2
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about/' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
